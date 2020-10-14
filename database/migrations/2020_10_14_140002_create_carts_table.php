1f32af196ea190f0ce9e383cfeca26d7299c0b4a
solomon <odumahs@gmail.com>
2020-10-14 20:34:09 +0100

    add cart model and migration

---
+++ database/migrations/2020_10_14_140002_create_carts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCartsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('carts', function (Blueprint $table) {
+            $table->id();
+            $table->string('card_id');
+            $table->integer('pizza_id');
+            $table->integer('quantity');
+            $table->enum('size', ['S', 'M', 'L'])->default('S');
+            $table->double('price', 8, 2)->default(0.00);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('carts');
+    }
+}
